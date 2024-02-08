4dcc0ace32e067c6a21482588a9954351e347716
Djemchik <jemil1.2004@mail.ru>
2024-02-08 20:21:11 +0300

    First release

---
+++ backup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Функция для определения текущей даты в формате ЧЧММГГГГ
+get_date() {
+    date +'%d%m%Y'
+}
+
+# Функция для получения IP адреса через интернет-сервис
+get_ip_info() {
+    ip_info=$(curl -s ipinfo.io/"$1"/country)
+    if [ -z "$ip_info" ]; then
+        echo "Failed to get IP information"
+        exit 1
+    else
+        echo "$ip_info"
+    fi
+}
+
+get_ip_address() {
+    ip=$(curl -s ifconfig.co)
+    if [ -z "$ip" ]; then
+        echo "Failed to get IP address"
+        exit 1
+    else
+        ip_info=$(get_ip_info "$ip")
+        echo "$ip_info $ip"
+    fi
+}
+
+# Путь к папке, которую нужно архивировать
+SOURCE_DIR="/opt/outline"
+
+# Получаем текущую дату и форматируем её
+DATE=$(get_date)
+
+# Создаем имя архива
+ARCHIVE_NAME="backup_$DATE.zip"
+
+# Архивируем содержимое папки
+zip -r "$ARCHIVE_NAME" "$SOURCE_DIR"
+
+# Определяем IP адрес
+IP=$(get_ip_address)
+
+#Определение страны от пользователя
+Country="none"
+
+# Токен вашего бота в Telegram
+BOT_TOKEN="none"
+
+# ID чата или группы, в которую нужно отправить архив
+CHAT_ID="none"
+
+# URL-адрес для отправки сообщения в Telegram
+TELEGRAM_API_URL="https://api.telegram.org/bot$BOT_TOKEN/sendDocument"
+
+# Преобразуем заголовок (caption) в UTF-8
+CAPTION="IP address = $IP 
+$Country
+"
+
+# Отправляем архив в Telegram вместе с сообщением об IP адресе отправителя
+curl -X POST "$TELEGRAM_API_URL" \
+     -F "chat_id=$CHAT_ID" \
+     -F "document=@$ARCHIVE_NAME" \
+     -F "caption=$CAPTION" \
+     --header "Content-Type: multipart/form-data; charset=utf-8"
+
+# Удаляем временный архив
+rm "$ARCHIVE_NAME"
---
+++ install.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+# Запрос прав суперпользователя
+sudo -v
+
+# Устанавливаем необходимые пакеты
+sudo apt update
+sudo apt install -y ntp zip jq
+
+# Запрос BOT_TOKEN
+read -p "Введите BOT_TOKEN: " BOT_TOKEN
+read -p "Введите страну нахождения сервера: " Country
+
+# Запрашиваем список чатов
+response=$(curl -s "https://api.telegram.org/bot$BOT_TOKEN/getUpdates")
+
+# Проверка наличия ошибок в ответе
+if echo "$response" | jq -e '.ok == false' >/dev/null; then
+    echo "Ошибка: Не удалось получить список чатов."
+    exit 1
+fi
+
+# Получаем информацию о чатах
+chat_info=$(echo "$response" | jq -r '.result[].message.chat | "\(.id) \(.title)"')
+
+# Инициализируем переменную для номера чата
+chat_number=1
+
+# Выводим доступные чаты
+echo "Доступные чаты:"
+echo "$chat_info" | while IFS= read -r line; do
+    echo "[$chat_number] $line"
+    chat_number=$((chat_number+1))
+done
+
+# Предоставление пользователю выбора чата или возможности вручную ввести CHAT_ID
+read -p "Выберите чат (введите номер из списка или введите 0, чтобы ввести вручную): " selected_chat_number
+
+# Проверка, был ли введен 0 для вручную введенного CHAT_ID
+if [ "$selected_chat_number" -eq 0 ]; then
+    read -p "Введите CHAT_ID: " CHAT_ID
+else
+    # Получаем ID выбранного чата
+    selected_chat_id=$(echo "$chat_info" | sed -n "${selected_chat_number}p" | awk '{print $1}')
+    CHAT_ID="$selected_chat_id"
+fi
+
+# Скачиваем скрипт backup.sh
+wget -O /tmp/backup.sh https://github.com/Djemchik/Trash/raw/main/backup.sh
+
+
+
+# Устанавливаем Московскую временную зону
+sudo timedatectl set-timezone Europe/Moscow
+
+# Копируем скрипт в каталог /tmp
+cp /tmp/backup.sh /tmp/backup_modified.sh
+
+# Редактирование скрипта backup.sh
+sed -i "s/CHAT_ID=\"[^\"]*\"/CHAT_ID=\"$CHAT_ID\"/" /tmp/backup_modified.sh
+sed -i "s/BOT_TOKEN=\"[^\"]*\"/BOT_TOKEN=\"$BOT_TOKEN\"/" /tmp/backup_modified.sh
+sed -i "s/Country=\"[^\"]*\"/Country=\"$Country\"/" /tmp/backup_modified.sh
+
+# Проверка формата времени
+validate_time() {
+    local time_pattern="^[0-9]+$"
+    if ! [ "$1" -ge 0 ] || ! [ "$1" -le "$2" ]; then
+        echo "Ошибка: Неверный формат времени. $3 должны быть в диапазоне от 00 до $2."
+        exit 1
+    fi
+}
+
+chmod +x /tmp/backup_modified.sh
+
+
+# Запрос времени для запуска скрипта
+read -p "Введите часы (от 00 до 23): " CRON_HOUR
+validate_time "$CRON_HOUR" 23 "Часы"
+
+# Запрос времени для запуска скрипта
+read -p "Введите минуты (от 00 до 59): " CRON_MINUTE
+validate_time "$CRON_MINUTE" 59 "Минуты"
+
+# Создание службы systemd
+sudo tee /etc/systemd/system/backup_script.service > /dev/null <<EOF
+[Unit]
+Description=Backup Script
+
+[Service]
+Type=simple
+ExecStart=/bin/bash /tmp/backup_modified.sh
+StandardOutput=journal
+StandardError=journal
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Создание таймера systemd
+sudo tee /etc/systemd/system/backup_script.timer > /dev/null <<EOF
+[Unit]
+Description=Run backup_script.service every day at HH:MM
+
+[Timer]
+OnCalendar=*-*-* $CRON_HOUR:$CRON_MINUTE:00
+Persistent=true
+
+[Install]
+WantedBy=timers.target
+EOF
+
+# Перезагрузка конфигурации systemd
+sudo systemctl daemon-reload
+
+# Активация таймера
+sudo systemctl enable --now backup_script.timer
+
+# Проверка успешности активации
+if systemctl is-active --quiet backup_script.timer; then
+    echo "Таймер успешно настроен."
+else
+    echo "Ошибка: Не удалось активировать таймер."
+    exit 1
+fi
+
+# Проверка статуса службы
+if systemctl is-active --quiet backup_script.service; then
+    echo "Служба успешно запущена."
+else
+    echo "Служба успешно запущена Всё чётко."
+    
+fi
+
+echo "Установка завершена."
