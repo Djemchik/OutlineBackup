b4873cdfbd8cdcb33a83f0ae3e30ea72eabcb0e0
Djemchik <jemil1.2004@mail.ru>
2024-03-29 23:07:22 +0300

    March Update

--- install.sh
+++ install.sh
@@ -59,7 +59,7 @@ rm -rf /opt/backup_modified.sh
 # Попытка загрузить файл через curl с таймаутом 20 секунд
 echo "Попытка загрузки файла через curl..."
 # Скачиваем скрипт backup.sh
-curl --connect-timeout 20 -o /tmp/backup.sh https://github.com/Djemchik/OutlineBackup/raw/main/backup.sh
+wget -O /tmp/backup.sh https://github.com/Djemchik/OutlineBackup/raw/main/backup.sh
 
 
 # Проверяем успешность загрузки
